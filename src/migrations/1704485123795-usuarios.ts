a0c6863457ace349a6fb70a6c509c6f5fd8a0b69
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2024-01-05 18:50:36 -0300

    tabela usuarios criada

---
+++ src/migrations/1704485123795-usuarios.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class Usuarios1704485123795 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const table = new Table({
+            name: 'users',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'serial',
+                    isPrimary: true
+                },
+                {
+                    name: 'name',
+                    type: 'text'
+                },
+                {
+                    name: 'email',
+                    type: 'text',
+                    isUnique: true
+                },
+                {
+                    name: 'passwoard',
+                    type: 'text'
+                }
+            ]
+
+        })
+        await queryRunner.createTable(table)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users')
+    }
+
+}
